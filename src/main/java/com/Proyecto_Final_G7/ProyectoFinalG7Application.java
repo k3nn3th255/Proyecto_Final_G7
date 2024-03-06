5f7e93a1a1ceac72838153eb286c4f093187362e
ASROCK <ASROCK@DESKTOP-RL7J0JH>
2024-03-05 22:52:30 -0600

    Creacion 40% historias de usuario

---
+++ src/main/java/com/Proyecto_Final_G7/ProyectoFinalG7Application.java
@@ -0,0 +1,13 @@
+package com.Proyecto_Final_G7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalG7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalG7Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Proyecto_Final_G7/ProyectoFinalG7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Proyecto_Final_G7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalG7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
