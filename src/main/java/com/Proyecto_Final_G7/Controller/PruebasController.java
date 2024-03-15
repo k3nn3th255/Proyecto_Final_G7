a36ec3c19dfa573dbd7851db60a386569320d6bf
Sebastian <Sebawtisn@gmail.com>
2024-03-15 02:03:28 -0600

    Commit #8 se añade la pestania tests que tiene una preview del catalogo

---
+++ src/main/java/com/Proyecto_Final_G7/Controller/PruebasController.java
@@ -0,0 +1,31 @@
+package com.Proyecto_Final_G7.Controller;
+
+import com.Proyecto_Final_G7.service.CategoriaService;
+import com.Proyecto_Final_G7.service.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/pruebas")
+public class PruebasController {
+
+    @Autowired
+    private ProductoService productoService;
+
+    @Autowired
+    private CategoriaService categoriaService;
+
+    @GetMapping("/listado")
+    public String listado(Model model) {
+        var productos = productoService.getProductos(false);
+        var categorias = categoriaService.getCategorias(false);
+        model.addAttribute("productos", productos);
+        model.addAttribute("categorias", categorias);
+        return "/pruebas/listado";
+    }
+
+}
+
--- src/main/java/com/Proyecto_Final_G7/service/FirebaseStorageService.java
+++ src/main/java/com/Proyecto_Final_G7/service/FirebaseStorageService.java
@@ -7,14 +7,14 @@ public interface FirebaseStorageService {
     public String cargaImagen(MultipartFile archivoLocalCliente, String carpeta, Long id);
 
     //El BuketName es el <id_del_proyecto> + ".appspot.com"
-    final String BucketName = "techshop-vt-55a87.appspot.com";
+    final String BucketName = "proyectofinalg7-d7863.appspot.com";
 
     //Esta es la ruta básica de este proyecto Techshop
-    final String rutaSuperiorStorage = "techshop";
+    final String rutaSuperiorStorage = "Proyecto_Final_G7";
 
     //Ubicación donde se encuentra el archivo de configuración Json
     final String rutaJsonFile = "firebase";
     
     //El nombre del archivo Json
-    final String archivoJsonFile = "techshop-vt-55a87-firebase-adminsdk-x8if0-900a62b132.json";
+    final String archivoJsonFile = "proyectofinalg7-d7863-firebase-adminsdk-epedd-21d2d76fde.json";
 }
