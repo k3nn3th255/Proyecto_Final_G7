82651f909ba8b21b7811091dcf869d0bca034a46
ASROCK <ASROCK@DESKTOP-RL7J0JH>
2024-03-29 10:29:24 -0600

    Implementacion de la seguridad de la web en UsuarioservicedetailsImpl

--- src/main/java/com/Proyecto_Final_G7/service/Impl/UsuarioDetailsServiceImpl.java
+++ src/main/java/com/Proyecto_Final_G7/service/Impl/UsuarioDetailsServiceImpl.java
@@ -1,7 +1,59 @@
-
 package com.Proyecto_Final_G7.service.Impl;
 
+import com.Proyecto_Final_G7.dao.UsuarioDao;
+import com.Proyecto_Final_G7.domain.Rol;
+import com.Proyecto_Final_G7.domain.Usuario;
+import com.Proyecto_Final_G7.service.UsuarioDetailsService;
+import jakarta.servlet.http.HttpSession;
+import java.util.ArrayList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-public class UsuarioDetailsServiceImpl {
-    
+@Service("userDetailsService")
+public class UsuarioDetailsServiceImpl
+        implements
+        UsuarioDetailsService,
+        UserDetailsService {
+
+    @Autowired
+    private UsuarioDao usuarioDao;
+
+    @Autowired
+    private HttpSession session;
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+
+        Usuario usuario = usuarioDao.findByUsername(username);
+
+        if (usuario == null) {
+
+            throw new UsernameNotFoundException(username);
+
+        }
+        // si estamos aca si se encontro el usuario 
+
+        session.removeAttribute("usuarioImagen");
+        session.setAttribute("usuarioImagen", usuario.getRutaImagen());
+        //se deben crear los roles de seguridad 
+        var roles = new ArrayList<GrantedAuthority>();
+        for (Rol r : usuario.getRoles()) {
+            roles.add(new SimpleGrantedAuthority(r.getNombre()));
+
+        }
+        //Aca tenemos toda la info del usuario
+
+        return new User(usuario.getUsername(),
+                usuario.getPassword(),
+                roles);
+    }
 }
